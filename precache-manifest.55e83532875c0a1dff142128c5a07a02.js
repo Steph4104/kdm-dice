609b6950723547f04e91571b03db6dd6261278b7
steph4104 <stephanie4104@gmail.com>
2020-03-20 10:02:07 -0400

    Updates

---
+++ precache-manifest.55e83532875c0a1dff142128c5a07a02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d495d5e4f82d3856e23aded3cde33bc9",
+    "url": "/kdm-dice/index.html"
+  },
+  {
+    "revision": "95c17c6f23d84b7d5cb4",
+    "url": "/kdm-dice/static/css/main.abc94a2e.chunk.css"
+  },
+  {
+    "revision": "ba3604baf3aaef0493a9",
+    "url": "/kdm-dice/static/js/2.f1542bb6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kdm-dice/static/js/2.f1542bb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95c17c6f23d84b7d5cb4",
+    "url": "/kdm-dice/static/js/main.5869afd3.chunk.js"
+  },
+  {
+    "revision": "ff28185e06b88e2e3f7a",
+    "url": "/kdm-dice/static/js/runtime-main.d9af4252.js"
+  }
+]);
